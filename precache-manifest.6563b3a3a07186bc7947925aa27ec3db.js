7aac40640c03025e19ddc768c397a921720559c1
Raphael10101 <raphaelguedj@gmail.com>
2019-11-15 08:48:41 +0000

    Updates

---
+++ precache-manifest.6563b3a3a07186bc7947925aa27ec3db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5159a9c1a32b1dc7f538c9fafb364de8",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "b0e84d5eb7916339512c",
+    "url": "/todoapp/static/css/main.a63788db.chunk.css"
+  },
+  {
+    "revision": "b450adef915dc49f1508",
+    "url": "/todoapp/static/js/2.74fa3ba8.chunk.js"
+  },
+  {
+    "revision": "b0e84d5eb7916339512c",
+    "url": "/todoapp/static/js/main.a5cf953d.chunk.js"
+  },
+  {
+    "revision": "7933d7e3297737125066",
+    "url": "/todoapp/static/js/runtime-main.400abd00.js"
+  }
+]);
